62c61c997d4a9481b6f623a5a3f230a75e8fa603
chalund <114383740+chalund@users.noreply.github.com>
2023-11-07 16:24:55 +0100

    adds new e2e-test

---
+++ cypress/e2e/testing/login.cy.js
@@ -0,0 +1,16 @@
+const email = "hellouser@noroff.no";
+const password = "12345678"
+
+describe("Login test", () => {
+    it("should allow a valid user to login with valid credentials", () => {
+      cy.visit("/");
+      cy.wait(1000);
+      cy.get("#registerForm button[type=button]").contains("Login").click();
+      cy.wait(1000);
+      cy.get("#loginEmail").type(email, "{enter}");
+      cy.get("#loginPassword").type(password);
+      cy.get("#loginForm button[type=submit]").contains("Login").click();
+      cy.wait(1000);
+      cy.url().should("include", "profile");
+    });
+  });
